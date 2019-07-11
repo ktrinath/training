3442493fba2ea0955e4208ee00541e5cb806d642
ktrinath <sami.mani10@gmail.com>
2019-07-11 20:04:43 +0530

    new class added

---
+++ src/org/cts/green/Sample.java
@@ -0,0 +1,34 @@
+package org.cts.green;
+
+
+
+public class Sample {
+	private void empId() {
+		System.out.println("252525");
+	}
+private void empName() {
+	System.out.println("Sabari");
+}
+private void empDob() {
+	System.out.println("25.03.92");
+}
+private void empPhone() {
+	System.out.println("8124252525");
+}
+private void empEmail() {
+	System.out.println("mail2vipsabari@gmail.com");
+}
+private void empAddress() {
+	System.out.println("Saligramam, Chennai");
+}
+public static void main(String[] args) {
+	Sample qone=new Sample();
+	qone.empId();
+	qone.empName();
+	qone.empDob();
+	qone.empPhone();
+	qone.empEmail();
+	qone.empAddress();	
+}
+
+}
